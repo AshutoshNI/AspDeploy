4c827a67f2fb68f9c7cba2f58a254d9c4d53e91c
Ashutosh Sharma <ashutosh@niit-tech.com>
2017-08-01 03:57:17 -0700

    Complete App

---
+++ Myapp/Myapp/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Myapp
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write( "This is a demo-Test 44444 application with current datetime :-" +System.DateTime.Now.ToString());
+            
+           
+
+        }
+    }
+}
